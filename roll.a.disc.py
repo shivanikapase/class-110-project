669fc19e774059bfdba5e3fee1f66f8f6bb8c1ad
shivanikapase <97745082+shivanikapase@users.noreply.github.com>
2024-01-02 21:19:51 +0530

    Add files via upload

---
+++ roll.a.disc.py
@@ -0,0 +1,56 @@
+import random
+
+a = "b" 
+ 
+while a == b :
+ 
+ number = random.randint(1, 6)
+
+ if number == 1:
+  print("[-----]")
+  print("[     ]")
+  print("[   0 ]")
+  print("[     ]")
+  print("[-----]")
+  
+
+  if number == 2:
+   print("[-----]")
+   print("[0     ]")
+   print("[      ]")
+   print("[     0]")
+   print("[-----]")
+   
+
+  if number == 3:
+   print("[-----]")
+   print("[     ]")
+   print("[0 0 0]")
+   print("[     ]")
+   print("[-----]")
+   
+
+  if number == 4:
+   print("[-----]")
+   print("[0   0]")
+   print("[     ]")
+   print("[0   0]")
+   print("[-----]")
+   
+  if number == 5:
+   print("[-----]")
+   print("[0   0]")
+   print("[  0  ]")
+   print("[0   0]")
+   print("[-----]")
+
+  if number == 6:
+   print("[-----]")
+   print("[0 0 0]")
+   print("[     ]")
+   print("[0 0 0]")
+   print("[-----]")
+
+a= input("Press y to continue: ")
+
+print("\n")
